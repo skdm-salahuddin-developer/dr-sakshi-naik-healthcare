7b9f0b75aa093d258628959882e117cc7c5543b8
salahuddin k <salau.skdm.developer@gmail.com>
2026-09-21 12:40:22 +0530

    feat: create AboutPage component with healthcare and appointment booking sections

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -5,6 +5,7 @@ import {
   DoctorPatientCareSection,
   QualificationsExpertiseSection
 } from '../components/about/index.js';
+import { QualificationsSection } from '../components/home/index.js';
 import { 
   AppointmentBookingSection, 
   DoctorsTeamSection, 
@@ -20,13 +21,16 @@ export default function AboutPage({ onNavigateHome, onBookAppointment }) {
       {/* 2. About Doctor & Patient Care Philosophy Section (Welcome To Dr. Sakshi Naik) */}
       <DoctorPatientCareSection onBookAppointment={onBookAppointment} />
 
-      {/* 3. Qualifications, Expertise & Clinical Experience (Why Choose Our Medical Excellence) */}
+      {/* 3. Qualifications & Training Section (Expert Care for Women’s & Fertility Health) */}
+      <QualificationsSection onBookAppointment={onBookAppointment} />
+
+      {/* 4. Qualifications, Expertise & Clinical Experience (Why Choose Our Medical Excellence) */}
       <QualificationsExpertiseSection onBookAppointment={onBookAppointment} />
 
-      {/* 4. "Our Professionals We Care About You" - 4-Card Service Feature Grid */}
+      {/* 5. "Our Professionals We Care About You" - 4-Card Service Feature Grid */}
       <AboutCareSection onBookAppointment={onBookAppointment} />
 
-      {/* 5. Consultation & Appointment Booking Form (Have A Questions? Consult With Dr. Sakshi Naik) */}
+      {/* 6. Consultation & Appointment Booking Form (Have A Questions? Consult With Dr. Sakshi Naik) */}
       <AppointmentBookingSection onBookAppointment={onBookAppointment} />
 
       {/* 7. Medical Doctors Team Showcase */}
